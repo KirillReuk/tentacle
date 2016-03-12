fbac197de528129a5fb8d2a7b38bd145e3e5c0c1
Kirill Reuk <kirillreuk@gmail.com>
2016-03-12 17:41:58 +0500

    Initial commit

---
+++ PSD Parser/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Kit_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(702, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(814, 527);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PSD Parser/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageMagick;
+using PhotoshopFile;
+using PaintDotNet.Core;
+
+namespace Kit_Generator
+{
+    public partial class Form1 : Form
+    {
+        const string manufacturer = "Adidas";
+        const int countStart = 81;
+        const int countEnd = 95;
+        string imagePath = "C:\\Users\\Kirill\\Documents\\Uni\\tentacleporn\\Standard Pack v3.0\\Adidas\\Adidas 81-94.psd";
+        string imageSavePath = "C:\\Users\\Kirill\\Documents\\Uni\\tentacleporn\\Standard Pack v3.0\\Adidas\\Adidas 1-401.psd";
+        string imageSaveFolder = "C:\\Users\\Kirill\\Documents\\Uni\\tentacleporn\\Standard Pack v3.0\\Adidas\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            action();
+        }
+
+        private void action()
+        {
+            PsdFile image = new PsdFile();
+            if (!FilePathHasInvalidChars(imagePath))
+            {
+                image = new PsdFile(imagePath, Encoding.ASCII);
+                image.Save(imageSavePath, Encoding.ASCII);
+            }
+
+            int cow = 3;
+            for (int j = countStart; j < countEnd; ++j)
+            {
+                PsdFile newImage = new PsdFile(imageSavePath, Encoding.ASCII);
+                newImage.Layers.Clear();
+
+                newImage.Layers.Add(image.Layers[1]);
+                for (; (image.Layers[cow].Name != manufacturer + " " + j) && (image.Layers[cow].Name != manufacturer + j); cow++)
+                    newImage.Layers.Add(image.Layers[cow]);
+                newImage.Layers.Add(image.Layers[cow]);
+                newImage.Layers.Add(image.Layers[image.Layers.Count-2]);
+
+                newImage.Save(imageSaveFolder + manufacturer + " " + j + ".psd", Encoding.ASCII);
+                cow++;
+            }
+        }
+
+        public static bool FilePathHasInvalidChars(string path)
+        {
+            return (!string.IsNullOrEmpty(path) && path.IndexOfAny(System.IO.Path.GetInvalidPathChars()) >= 0);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int convertedInt;
+            if (Int32.TryParse(textBox1.Text, out convertedInt))
+            {
+                showLayer(convertedInt);
+            }
+        }
+
+        private void showLayer(int layerNumber)
+        {
+            
+            using (MagickImageCollection collection = new MagickImageCollection(imagePath))
+            {
+                MagickReadSettings settings = new MagickReadSettings();
+                
+                MagickImage image = collection[layerNumber];
+                MagickFormatInfo info = image.FormatInfo;
+
+                pictureBox.Image = image.ToBitmap();
+            }
+        }
+    }
+}
