0661b7be18494ba0ddc9a761795a02a80c68015a
Kirill Reuk <kirillreuk@gmail.com>
2016-11-09 14:08:12 +0500

    Fix #8
    Now rotation is correct

--- Kit Generator/Coloring.cs
+++ Kit Generator/Coloring.cs
@@ -241,22 +241,19 @@ namespace KitGenerator
 
         public static Bitmap RotateImage(Bitmap b, float angle)
         {
-            //Create a new empty bitmap to hold rotated image.
             Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
-            //Make a graphics object from the empty bitmap.
             Graphics g = Graphics.FromImage(returnBitmap);
-            //move rotation point to center of image.
+            
             g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+            
+            Rectangle coords = GetTrimmedCoordinates(b);
+            g.TranslateTransform(coords.X + coords.Width / 2, coords.Y + coords.Height / 2);
             //Rotate.        
             g.RotateTransform(angle);
             //Move image back.
-            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
-            //Draw passed in image onto graphics object.
-            //Found ERROR 1: Many people do g.DwarImage(b,0,0); The problem is that you re giving just the position
-            //Found ERROR 2: Many people do g.DrawImage(b, new Point(0,0)); The size still not present hehe :3
+            g.TranslateTransform(-coords.X - coords.Width / 2, -coords.Y - coords.Height / 2);
 
-            g.DrawImage(b, 0, 0, b.Width, b.Height);  //My Final Solution :3
+            g.DrawImage(b, 0, 0, b.Width, b.Height);
             return returnBitmap;
         }
 
